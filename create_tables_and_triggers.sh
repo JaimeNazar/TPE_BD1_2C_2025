3b1ce5b8fb3bd1677bcfedad836241fd6996658e
Jaime Nazar Anchorena <jnazaranchorena@itba.edu.ar>
2025-11-18 10:13:25 -0300

    fix: Scripts

--- create_tables_and_triggers.sh
+++ create_tables_and_triggers.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-if ( $# != 1 ); then
-    echo "Usage: ./create_tables_and_triggers.sh [username]"
-fi
-
 # Crear las tablas
-psql -h bd1.it.itba.edu.ar -U $1 -f tables.sql PROOF
+psql -h bd1.it.itba.edu.ar -U $USER -f tables.sql PROOF
 
 # Crear los triggers
-psql -h bd1.it.itba.edu.ar -U $1 -f triggers.sql PROOF
+psql -h bd1.it.itba.edu.ar -U $USER -f triggers.sql PROOF
--- insert_data.sh
+++ insert_data.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-if ( $# != 1 ); then
-    echo "Usage: ./create_tables_and_triggers.sh [username]"
-fi
-
 # Insertar todas las tuplas
 \copy pago from pagos.csv header delimiter ',' csv;
